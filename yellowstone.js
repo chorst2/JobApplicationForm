9e04834b24dce60775c6ac7a74d7642f892b4cff
Casidy Horst <CHorst2@my.wctc.edu>
2021-09-07 16:55:03 -0500

    Initial commit

---
+++ yellowstone.js
@@ -0,0 +1,20 @@
+
+$("#addEmployer2").on("click", function(){
+    $("#employer2").removeClass("d-none");
+});
+
+$("#addEmployer3").on("click", function(){
+    $("#employer3").removeClass("d-none");
+});
+
+$("#yesInfo1Radio").on("click", function(){
+    $("#ifYes1").removeClass("d-none");
+});
+
+$("#yesInfo2Radio").on("click", function(){
+    $("#ifYes2").removeClass("d-none");
+});
+
+$("#yesInfo3Radio").on("click", function(){
+    $("#ifYes3").removeClass("d-none");
+});
